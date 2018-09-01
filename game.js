e88f4d54efb21aed820e26c665bfa928ea7246eb
Constantine <bondaletov@gmail.com>
2018-09-02 00:15:16 +0300

    Vector.plus

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
 'use strict';
+class Vector {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    plus(vector) {
+        if (!(vector instanceof Vector)) {
+            throw new SyntaxError("ошибка"); // почему тут try-catch не сработал в тестах? функционал такой же
+        }
+        return new Vector(this.x + vector.x, this.y + vector.y);
+    }
+}
