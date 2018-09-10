c75d280a772cf3427c12145574dd350e282cd941
Constantine <bondaletov@gmail.com>
2018-09-10 16:02:58 +0300

    deploy

--- game.js
+++ game.js
@@ -18,12 +18,8 @@ class Vector {
     }
 
     times(n) {
-        // console.log(this)
-        // console.log(n)
         const x = this.x * n;
         const y = this.y * n;
-        // console.log(x,y)
-        // console.log(`------------------\n\n`)
         return new Vector(x, y);
     }
 }
@@ -125,10 +121,6 @@ class Level {
         return maxWidth;
     }
 
-    // get player() {
-    //     return (this.actors).find(el => el.title = "Игрок");
-    // }
-
     isFinished() {
         if (this.status && this.finishDelay < 0) return true;
         return false;
@@ -146,7 +138,7 @@ class Level {
         });
     }
 
-    obstacleAt(position, size) { // #FIXME
+    obstacleAt(position, size) {
         if ((false === position instanceof Vector) || (false === size instanceof Vector)) throw new Error;
         
 
@@ -168,14 +160,6 @@ class Level {
                 }
             }
         }
-
-        // if (position.x < 0) return 'wall';
-        // if (position.x + size.x > this.width) return 'wall';
-        // if (position.y < 0) return 'wall';
-        // if (position.y + size.y > this.height) return 'lava';
-
-        // if (this.grid[position.y][position.x] === 'wall') return 'wall';
-        // if (this.grid[position.y][position.x] === 'lava') return 'lava';
     }
 
     removeActor(actor) {
@@ -200,7 +184,6 @@ class Level {
         if (barrierStr === 'lava' || barrierStr === 'fireball') this.status = 'lost';
 
         if (barrierStr === 'coin' && actor.type === 'coin') {
-        // if (barrierStr === 'coin' && (actor instanceof Actor || actor.__proto__ instanceof Actor)) {
             this.removeActor(actor);
             if (this.noMoreActors('coin')) this.status = 'won';
         }
@@ -240,12 +223,9 @@ class LevelParser {
         }
 
         return grid;
-        // console.log(this.dictionaryObstacles)
-        // return plan.map(row => row.split('').map(cell => this.dictionaryObstacles[cell]));
     }
 
     createActors(actorsKeys) {
-        // if (actorsKeys.length === 0 || this.dictionaryActors === undefined) return [];
         if (!Array.isArray(actorsKeys)) {
             return;
         }
@@ -265,45 +245,7 @@ class LevelParser {
         });
         return actors;
     }
-    // createActors(actors) {
-    //     if (actors.length === 0 || this.dictionaryActors === undefined) return [];
-
-    //     const checkActors = actors.every(row => {
-    //         const tmp = row.split('');
-    //         return tmp.every(cell => {
-    //             if (cell === " " || this.dictionaryActors[cell] !== undefined || this.dictionaryObstacles[cell] !== undefined) {
-    //                 return true;
-    //             }
-    //             return false;
-    //         })
-    //     });
-    //     if (!checkActors) return [];
-
-    //     const checkFuncAndActor = actors.every(row => {
-    //         const tmp = row.split('');
-    //         return tmp.every(cell => {
-    //             if (cell === " " || (typeof this.dictionaryActors[cell] === 'function' && (this.dictionaryActors[cell].name === "Actor" || this.dictionaryActors[cell].prototype instanceof Actor)) || this.dictionaryObstacles[cell] !== undefined) {
-    //                 return true;
-    //             }
-    //             return false;
-    //         })
-    //     });
-    //     if (!checkFuncAndActor) return [];
-
-    //     let resultActorsArray = [];
-    //     actors.forEach((row, rowIdx) => {
-    //         const tmp = row.split('');
-    //         tmp.forEach((cell, cellIdx) => {
-    //             if (cell === " " || this.dictionaryObstacles[cell] !== undefined) return;
-
-    //             const position = new Vector(cellIdx, rowIdx);
-    //             resultActorsArray.push(new this.dictionaryActors[cell](position));
-    //         })
-    //     });
-
-    //     return resultActorsArray;
-    // }
-
+ 
     parse(plan) {
         let grid = this.createGrid(plan);
         let actors = this.createActors(plan);
@@ -323,33 +265,22 @@ class Fireball extends Actor {
 
     getNextPosition(time = 1) {
         return this.pos.plus(this.speed.times(time));
-        // for (var i = 0; i < t; i++) {
-        //     this.pos.x = this.pos.x + this.speed.x;
-        //     this.pos.y = this.pos.y + this.speed.y;
-        // }
-        // this.pos = this.pos.plus(this.speed)
-        // return this.pos;
     }
 
     handleObstacle() {
         this.speed.x = -this.speed.x;
         this.speed.y = -this.speed.y;
-        // console.log(this.speed)
-        // this.speed.x *= -1;
-        // this.speed.y *= -1;
     }
 
     act(time, level) {
         let nextPosition = this.getNextPosition(time);
         const obstacle = level.obstacleAt(nextPosition, this.size);
-        // console.log(obstacle)
 
         if (obstacle !== undefined) {
             this.handleObstacle();
         } else {
             this.pos = nextPosition;
         }
-
     }
 }
 
@@ -362,7 +293,6 @@ class HorizontalFireball extends Fireball {
 
 class VerticalFireball extends Fireball {
     constructor(pos) {
-        // const speed = new Vector(0, 2);
         super(pos, new Vector(0, 2));
     }
 }
